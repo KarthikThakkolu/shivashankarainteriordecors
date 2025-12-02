3153e756772b5d409ad8756f0672a736df9d88a1
Karthik Thakkolu <150405840+KarthikThakkolu@users.noreply.github.com>
2025-12-02 23:06:11 +0530

    Add files via upload

---
+++ feedbackmodule.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const feedbackSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  rating: { type: Number, required: true },
+  message: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now }, // This is the correct field
+});
+
+module.exports = mongoose.model("Feedback", feedbackSchema);
